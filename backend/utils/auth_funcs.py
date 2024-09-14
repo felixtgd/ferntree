6f5c72ad638c9a64c4f4a84a0a60e59d1dadba58
felixtdg <felix.tangerding@gmx.de>
2024-09-14 17:41:21 +0200

    remove mypy.ini and correct wrapper types

--- backend/utils/auth_funcs.py
+++ backend/utils/auth_funcs.py
@@ -1,41 +1,6 @@
-# from functools import wraps
-# from fastapi import HTTPException, status
-# from typing import Optional, Any, Callable, TypeVar
-
-# from backend.database.mongodb import MongoClient
-
-# # Define a generic type for the decorated function
-# F = TypeVar("F", bound=Callable[..., Any])
-
-
-# def check_user_exists(db_client: MongoClient) -> Callable[[F], F]:
-#     def decorator(func: F) -> F:
-#         @wraps(func)
-#         async def wrapper(*args, **kwargs) -> Any:
-#             user_id: Optional[str] = kwargs.get("user_id")
-
-#             if user_id is None:
-#                 raise HTTPException(
-#                     status_code=status.HTTP_400_BAD_REQUEST,
-#                     detail="User ID is required.",
-#                 )
-
-#             user_exists: bool = await db_client.check_user_exists(user_id)
-#             if not user_exists:
-#                 raise HTTPException(
-#                     status_code=status.HTTP_404_NOT_FOUND,
-#                     detail=f"User with ID {user_id} not found.",
-#                 )
-
-#             return await func(*args, **kwargs)
-
-#         return wrapper
-
-#     return decorator
-
 from functools import wraps
 from fastapi import HTTPException, status
-from typing import Any, Callable, TypeVar, Awaitable
+from typing import Any, Callable, TypeVar, Awaitable, Optional
 
 from backend.database.mongodb import MongoClient
 
@@ -47,7 +12,7 @@ def check_user_exists(db_client: MongoClient) -> Callable[[F], F]:
     def decorator(func: F) -> F:
         @wraps(func)
         async def wrapper(*args: Any, **kwargs: Any) -> Any:
-            user_id: str | None = kwargs.get("user_id")
+            user_id: Optional[str] = kwargs.get("user_id")
 
             if user_id is None:
                 raise HTTPException(
