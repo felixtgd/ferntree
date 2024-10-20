cdce46ae76add6231157f5ec16ac2be0f978d85d
felixtdg <felix.tangerding@gmx.de>
2024-10-20 20:49:41 +0200

    add arrow to create model if no models yet

--- app/workspace/models/page.tsx
+++ app/workspace/models/page.tsx
@@ -4,6 +4,37 @@ import { fetchModels } from "@/app/utils/helpers";
 import { ModelData } from "@/app/utils/definitions";
 
 
+function EmptyStateMessage() {
+    return (
+      <div className="flex relative">
+        <svg
+            className="absolute top-12 right-32 w-128 h-64 text-blue-500"
+            fill="none"
+            viewBox="0 0 100 50" // "min-x min-y width height"
+            stroke="currentColor"
+        >
+            <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1}
+            d="M0 35 C45 35, 85 25, 95 5 L89 10 M95 5 L93 13"
+            />
+            <text
+                x="1"
+                y="45"
+                fill="currentColor"
+                fontSize="4"
+                // fontFamily="Arial, sans-serif"
+                fontWeight="300"
+                strokeWidth="0.2"
+            >
+                Start by creating a model
+            </text>
+        </svg>
+      </div>
+    );
+  }
+
 export default async function Page() {
 
     const models: ModelData[] = await fetchModels();
@@ -21,11 +52,15 @@ export default async function Page() {
                 </div>
             </div>
 
-            <div>
+            {models.length === 0 ? (
+                <EmptyStateMessage />
+            ) : (
+                <div>
                 {models.map((modelData, index) => (
                     <ModelCard key={index} modelData={modelData} />
                 ))}
-            </div>
+                </div>
+            )}
         </main>
     );
 }
