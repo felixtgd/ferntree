49ecf2813a2e76c362e6de88992cbd8060c760d9
felixtdg <felix.tangerding@gmx.de>
2024-10-26 23:57:27 +0200

    add docstrings

--- backend/clean_database.py
+++ backend/clean_database.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from database.mongodb import MongoClient
+from backend.database.mongodb import MongoClient
 
 # Create a MongoDB client
 db_client: MongoClient = MongoClient()
@@ -10,6 +10,12 @@ collections: list[str] = ["model_specs", "sim_evaluation", "sim_timeseries"]
 
 
 async def clean_collections(collections: list[str]) -> None:
+    """Clean the collections in the database.
+
+    Args:
+        collections (list[str]): The list of collections to clean.
+
+    """
     # Delete all documents in the collections
     for collection in collections:
         await db_client.clean_collection(collection)
--- backend/utils/auth_funcs.py
+++ backend/utils/auth_funcs.py
@@ -1,6 +1,7 @@
 from functools import wraps
+from typing import Any, Awaitable, Callable, Optional, TypeVar
+
 from fastapi import HTTPException, status
-from typing import Any, Callable, TypeVar, Awaitable, Optional
 
 from backend.database.mongodb import MongoClient
 
@@ -9,6 +10,39 @@ F = TypeVar("F", bound=Callable[..., Awaitable[Any]])
 
 
 def check_user_exists(db_client: MongoClient) -> Callable[[F], F]:
+    """A decorator that checks if a user exists in the database before
+    executing the decorated function.
+
+    This decorator is designed to be used with FastAPI route handlers.
+    It expects the decorated function to have a 'user_id' parameter, either as a
+    keyword argument or as part of a Pydantic model passed to the function.
+
+    Args:
+        db_client (MongoClient): An instance of the MongoDB client used to
+                                    check user existence.
+
+    Returns:
+        Callable[[F], F]: A decorator function that wraps the original function.
+
+    Raises:
+        HTTPException:
+            - 400 Bad Request if the user_id is not provided in the function arguments.
+            - 404 Not Found if the user with the given ID does not exist in the database
+
+    Usage:
+        @app.get("/some-route")
+        @check_user_exists(db_client)
+        async def some_route_handler(user_id: str):
+            # Function implementation
+
+    Note:
+        - The decorated function must be an async function.
+        - The 'user_id' parameter is expected to be a string.
+        - This decorator adds overhead to each request as it performs a database check.
+          Consider caching or other optimization techniques for high-traffic scenarios.
+
+    """
+
     def decorator(func: F) -> F:
         @wraps(func)
         async def wrapper(*args: Any, **kwargs: Any) -> Any:
